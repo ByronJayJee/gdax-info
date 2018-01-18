b3d83dca6cb6899c480fec0f887e0dc541e456dc
ByronJayJee <bgask001@fiu.edu>
2018-01-18 03:18:32 +0000

    made new routine gdax_global_data. Grabs product data from gdax and stores as global variable

--- gdax_test.py
+++ gdax_test.py
@@ -1,10 +1,11 @@
 import gdax
+import gdax_global_data as ggd
 import pprint
 import json
 
-public_client = gdax.PublicClient()
+#public_client = gdax.PublicClient()
 
-product_list = public_client.get_products()
+#product_list = public_client.get_products()
 #pprint.pprint(product_list)
 
-print(json.dumps(product_list, sort_keys=True, indent=4))
+print(json.dumps(ggd.product_list, sort_keys=True, indent=4))
