c6d5be2644417fcabf52350180368d284f03d8e1
ByronJayJee <bgask001@fiu.edu>
2018-01-18 02:38:34 +0000

    initial commit

---
+++ gdax_test.py
@@ -0,0 +1,10 @@
+import gdax
+import pprint
+import json
+
+public_client = gdax.PublicClient()
+
+product_list = public_client.get_products()
+#pprint.pprint(product_list)
+
+print(json.dumps(product_list, sort_keys=True, indent=4))
