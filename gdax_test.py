dedff0f0ae8d946ab4b4d9940f150a06dc06c68f
ByronJayJee <ByronJayJee@gmail.com>
2018-01-19 06:20:50 +0000

    used gdax-python websocket to get live data feed

--- gdax_test.py
+++ gdax_test.py
@@ -1,11 +1,32 @@
 import gdax
+import time
 import gdax_global_data as ggd
 import pprint
 import json
 
-#public_client = gdax.PublicClient()
+class myWebsocketClient(gdax.WebsocketClient):
+    def on_open(self):
+        self.url = "wss://ws-feed.gdax.com/"
+        self.products = ["LTC-USD"]
+        self.message_count = 0
+        print("Lets count the messages!")
+    def on_message(self, msg):
+        self.message_count += 1
+        if 'price' in msg and 'type' in msg:
+            print ("Message type:", msg["type"],
+                   "\t@ {:.3f}".format(float(msg["price"])))
+    def on_close(self):
+        print("-- Goodbye! --")
 
 #product_list = public_client.get_products()
 #pprint.pprint(product_list)
 
 print(json.dumps(ggd.product_list, sort_keys=True, indent=4))
+
+wsClient = myWebsocketClient()
+wsClient.start()
+print(wsClient.url, wsClient.products)
+while (wsClient.message_count < 5):
+    print ("\nmessage_count =", "{} \n".format(wsClient.message_count))
+    time.sleep(1)
+wsClient.close()
