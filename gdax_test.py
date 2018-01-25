95acc0ba15d820b7163202959e4afc8e8ab412fa
ByronJayJee <ByronJayJee@gmail.com>
2018-01-25 04:23:36 +0000

    websocket only prints prices for match transactions. added simple product ticker.

--- gdax_test.py
+++ gdax_test.py
@@ -12,11 +12,15 @@ class myWebsocketClient(gdax.WebsocketClient,object):
         print("Lets count the messages!")
     def on_message(self, msg):
         self.message_count += 1
-        if 'price' in msg and 'type' in msg:
+        if 'price' in msg and 'type' in msg and msg["type"]=="match":
+            print(msg)
             print ("Message type:", msg["type"],
                    "\t@ {:.3f}".format(float(msg["price"])))
     def on_close(self):
         print("-- Goodbye! --")
+
+
+public_client = gdax.PublicClient()
 
 #product_list = public_client.get_products()
 #pprint.pprint(product_list)
@@ -28,7 +32,15 @@ myProd = ["LTC-USD"]
 wsClient = myWebsocketClient(products=myProd)
 wsClient.start()
 print(wsClient.url, wsClient.products)
-while (wsClient.message_count < 50):
+while (wsClient.message_count < 5000):
     print ("\nmessage_count =", "{} \n".format(wsClient.message_count))
     time.sleep(1)
 wsClient.close()
+'''
+ticker_count=0
+while (ticker_count < 5):
+    prod_ticker = public_client.get_product_ticker(product_id='ETH-USD')
+    time.sleep(3)
+    print(prod_ticker)
+    ticker_count += 1
+'''
