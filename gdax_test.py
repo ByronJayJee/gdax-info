a584fb226e6d0866d9fe4b7873758814ffd5af16
ByronJayJee <ByronJayJee@gmail.com>
2018-08-21 05:08:59 +0000

    Save transaction stream in pandas data frame. Then take that dataframe and do some basic datetime resampling and averaging.

--- gdax_test.py
+++ gdax_test.py
@@ -1,6 +1,7 @@
 import gdax
 import time
 import gdax_global_data as ggd
+import pandas as pd
 import pprint
 import json
 
@@ -9,13 +10,18 @@ class myWebsocketClient(gdax.WebsocketClient,object):
         self.url = "wss://ws-feed.gdax.com/"
         #self.products = ["LTC-USD"]
         self.message_count = 0
+        # Create class object that is Pandas DataFrame
+        self.df = pd.DataFrame(columns=['time', 'trade_id', 'price'])
         print("Lets count the messages!")
     def on_message(self, msg):
         self.message_count += 1
         if 'price' in msg and 'type' in msg and msg["type"]=="match":
-            print(msg)
+            #print(msg)
             print ("Message type:", msg["type"],
                    "\t@ {:.3f}".format(float(msg["price"])))
+            # Append message to dataframe
+            # This use of pandas dataframe is VERY inefficient!!! Come up with something better for long-term
+            self.df = self.df.append({'time': msg['time'], 'trade_id': msg['trade_id'], 'price': msg['price']}, ignore_index=True)
     def on_close(self):
         print("-- Goodbye! --")
 
@@ -27,15 +33,52 @@ public_client = gdax.PublicClient()
 
 #print(json.dumps(ggd.product_list, sort_keys=True, indent=4))
 
-myProd = ["LTC-USD"]
+#myProd = ["LTC-USD"]
+myProd = ["BTC-USD"]
 
 wsClient = myWebsocketClient(products=myProd)
 wsClient.start()
 print(wsClient.url, wsClient.products)
-while (wsClient.message_count < 5000):
-    print ("\nmessage_count =", "{} \n".format(wsClient.message_count))
-    time.sleep(1)
+minute_count=0
+#while (wsClient.message_count < 5000):
+while (minute_count < 30):
+    #print ("\nmessage_count =", "{} \n".format(wsClient.message_count))
+    print ("\nminute_count =", "{} \n".format(minute_count))
+    time.sleep(60)
+    minute_count += 1
 wsClient.close()
+
+print(wsClient.df)
+
+# Convert time property to an actual timestamp
+wsClient.df['time'] = pd.to_datetime(wsClient.df['time'])
+
+# use the timestamp as dataframe index
+# this effectievely makes this a time series
+wsClient.df.index = wsClient.df['time']
+
+print(wsClient.df.dtypes)
+
+# Convert the 'trade_id' and 'price' columns to type: float
+d=wsClient.df.astype({'trade_id':float, 'price':float})
+print(d.dtypes)
+print(d)
+
+# resample data into 1 minute intervals
+r=d.resample('1min')
+#r=d.resample('5S')
+
+# forward fill missing data in resampled dataframe
+rl_fill = r.last().ffill()
+
+# compute moving average of price data in resampled dataframe
+rl_fill['ma'] = rl_fill['price'].rolling(9).mean()
+
+# compute difference in price and moving average in resampled dataframe
+rl_fill['diff'] = rl_fill['price']-rl_fill['ma']
+print(r)
+print(rl_fill)
+
 '''
 ticker_count=0
 while (ticker_count < 5):
